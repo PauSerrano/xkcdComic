e2ab8c4d9c60c1e9c053d1bacd227b70302b8ac3
Pau <paserrod@gmail.com>
2017-01-11 18:21:33 +0100

    Solucion del ejercicio
    Descarga de Json y obtención de la url de la imagen.
    Incluido botón para crear nuevos Json aleatorios con su
    correspondientes imagenes

--- app/src/main/java/es/schooleando/xkcdcomic/ComicActivity.java
+++ app/src/main/java/es/schooleando/xkcdcomic/ComicActivity.java
@@ -1,32 +1,106 @@
 package es.schooleando.xkcdcomic;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.os.Handler;
-import android.os.ResultReceiver;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import java.util.Random;
 
 public class ComicActivity extends AppCompatActivity implements BgResultReceiver.Receiver {
+
+    //Primero damos ****** PERMISOS ******* A LA APP para acceder a la red en el Manifest
+
+    private static final String LOGTAG = "xkcdCommic_MainActivity";
+
     private BgResultReceiver mResultReceiver;
+
+    private TextView tvUrl;
+    private TextView tvProgreso;
+    private ImageView ivImagen;
+    private ProgressBar barraProgreso;
+    private Button boton;
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_comic);
 
+        tvUrl = (TextView) findViewById(R.id.textView);
+        tvProgreso = (TextView) findViewById(R.id.textView2);
+        ivImagen = (ImageView) findViewById(R.id.imageView);
+        barraProgreso = (ProgressBar) findViewById(R.id.progressBar);
+        boton = (Button) findViewById(R.id.button);
+
+        //** Creamos un objeto MiResultReceiver **
         mResultReceiver = new BgResultReceiver(new Handler());
         mResultReceiver.setReceiver(this);
+
+        barraProgreso.setMax(100);
+        barraProgreso.setProgress(0);
+
+        Log.d(LOGTAG, "Lanzamos el intent del servicio");
 
         // Esto es gratis: al arrancar debemos cargar el cómic actual
         Intent intent = new Intent(this, DownloadIntentService.class);
         intent.putExtra("url", "http://xkcd.com/info.0.json");
         intent.putExtra("receiver", mResultReceiver);
         startService(intent);
+
+
+
+
     }
 
 
     @Override
     public void onReceiveResult(int resultCode, Bundle resultData) {
+
+        Log.d(LOGTAG, "Estamos en ReceiverResult");
+
+        int progreso = 0;
+
+        switch (resultCode){
+            case DownloadIntentService.PROGRESS:
+                Log.d(LOGTAG, "Estamos en ReceiverResult_PROGRESO");
+
+
+                barraProgreso.setProgress(Integer.parseInt(resultData.getString("ServicioDescarga")));
+                tvProgreso.setText(resultData.getString("ServicioDescarga"));
+
+
+                break;
+            case DownloadIntentService.FINSISHED:
+                Log.d(LOGTAG, "Estamos en ReceiverResult_FINALIZADO");
+
+                ivImagen.setImageBitmap((Bitmap) resultData.get("bitmapImagen"));
+
+                break;
+            case DownloadIntentService.ERROR:
+                Log.d(LOGTAG, "Estamos en ReceiverResult_LOG");
+
+                Toast.makeText(this, "Error al descargar la imagen", Toast.LENGTH_SHORT).show();
+                break;
+            case DownloadIntentService.INICIO:
+
+                tvUrl.setText((resultData.getString("urlImg")));
+                //tvUrl.setText("comprobando");
+                break;
+
+
+        }
+
+
         // TODO: podemos recibir diferentes resultCodes del IntentService
         //      ERROR -> ha habido un problema de la conexión (Toast)
         //      PROGRESS -> nos estamos descargando la imagen (ProgressBar)
@@ -36,5 +110,23 @@ public class ComicActivity extends AppCompatActivity implements BgResultReceiver
 
     }
 
+    public void descargarOtra(View view) {
+
+        Random random = new Random();
+
+        int numComic = random.nextInt(1000);
+
+        String urlImagenNueva = "http://xkcd.com/"+numComic+"/info.0.json";
+
+        // Esto es gratis: al arrancar debemos cargar el cómic actual
+        Intent intent = new Intent(this, DownloadIntentService.class);
+        intent.putExtra("url", urlImagenNueva);
+        intent.putExtra("receiver", mResultReceiver);
+        startService(intent);
+
+    }
+
     // TODO: Falta un callback de ImageView para hacer click en la imagen y que se descargue otro comic aleatorio.
+
+
 }
--- app/src/main/java/es/schooleando/xkcdcomic/DownloadIntentService.java
+++ app/src/main/java/es/schooleando/xkcdcomic/DownloadIntentService.java
@@ -3,13 +3,47 @@ package es.schooleando.xkcdcomic;
 import android.app.IntentService;
 import android.content.Intent;
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.net.ConnectivityManager;
+import android.net.NetworkInfo;
 import android.os.Bundle;
 import android.os.ResultReceiver;
+import android.os.SystemClock;
+import android.util.JsonReader;
 import android.util.Log;
 
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.io.BufferedInputStream;
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
+import java.net.HttpURLConnection;
+import java.net.MalformedURLException;
+import java.net.ProtocolException;
+import java.net.SocketTimeoutException;
+import java.net.URL;
+
+import static org.apache.http.params.CoreConnectionPNames.CONNECTION_TIMEOUT;
+
 public class DownloadIntentService extends IntentService  {
+
+    // definimos tipos de mensajes que utilizaremos en ResultReceiver
+    public static final int PROGRESS = 0;
+    public static final int FINSISHED = 1;
+    public static final int ERROR = 2;
+    public static final int INICIO = 3;
+
+
+
     private static final String TAG = DownloadIntentService.class.getSimpleName();
     private ResultReceiver mReceiver;
+    private String urlImagenString;
+    private Bitmap imagenBmpDescargada;
 
     public DownloadIntentService() {
         super("DownloadIntentService");
@@ -17,20 +51,143 @@ public class DownloadIntentService extends IntentService  {
 
     @Override
     protected void onHandleIntent(Intent intent) {
-        mReceiver = intent.getParcelableExtra("receiver");
+
+
         Log.d(TAG, "onHandleIntent");
 
-        // TODO Aquí hacemos la conexión y accedemos a la imagen.
-        //
-        // TODO: Habrá que hacer 2 conexiones:
-        //  1. Para descargar el resultado JSON para leer la URL.
-        //  2. Una vez tenemos la URL descargar la imagen en la carpeta temporal.
+
+        if (intent != null) {
+            //++ Creamos un receptor de resultados recogido del intent que a dado paso al inicio del servicio ++
+            mReceiver = intent.getParcelableExtra("receiver");
+
+            //Obtenemos la url del intent
+            String urlStringJson = intent.getStringExtra("url");
+
+            Log.d(TAG, "obtnida la url: " + urlStringJson);
 
 
-        // TODO: Devolver la URI de la imagen si todo ha ido bien.
+            try {
+                // TODO Aquí hacemos la conexión y accedemos a la imagen.
+                //Abrimos la conexion
+                // TODO: Habrá que hacer 2 conexiones:
+                //  1. Para descargar el resultado JSON para leer la URL.
+                URL url = new URL(urlStringJson);
+                HttpURLConnection urlConnection = (HttpURLConnection) url.openConnection();
 
-        // TODO: Controlar los casos en los que no ha ido bien: excepciones en las conexiones, etc...
+                //Realizamos la peticion de la descarga del Json
+                urlConnection.setRequestMethod("GET");
+                urlConnection.connect();
+                InputStream inJson = new BufferedInputStream(urlConnection.getInputStream());
+                //Guardamos el json en un objeto JsonReader para facilitar la lectura en el caso qeu fueran varios
+                JsonReader jsonReader = new JsonReader(new InputStreamReader(inJson, "UTF-8"));
+                //metodo creado para obtener la url de la imagen que contiene el json obtenido del inputStream
+                urlImagenString = leerJson(jsonReader);
+                jsonReader.close();
+                //Por comprobacion enviamos la url de la imagen obtenida
+                Bundle b = new Bundle();
+                b.putString("urlImg", urlImagenString);
+                mReceiver.send(this.INICIO, b);
 
-        mReceiver.send(0, Bundle.EMPTY);  // cambiar
+                //  2. Una vez tenemos la URL descargar la imagen en la carpeta temporal.
+                URL urlImagen = new URL(urlImagenString);
+                HttpURLConnection urlConnectionDescarga = (HttpURLConnection) urlImagen.openConnection();
+
+                //Realizamos la peticion de la descarga de la imagen
+                urlConnectionDescarga.setRequestMethod("GET");
+                urlConnectionDescarga.connect();
+                String tipo = urlConnectionDescarga.getContentType();
+                int tamañoRecurso = urlConnectionDescarga.getContentLength();
+
+                if (tipo.startsWith("image/")) {
+                    InputStream is = urlConnectionDescarga.getInputStream();
+                    ByteArrayOutputStream bos = new ByteArrayOutputStream();
+
+                    byte[] buffer = new byte[1024];
+                    int n = 0;
+                    int total = 0;
+                    //Mientras el resultado de lectura del buffer sea distinto a -1
+                    Log.d(TAG, "Empieza la descarga de la imagen");
+                    while ((n = is.read(buffer)) != -1) {
+                        //Escribimos los bytes
+                        bos.write(buffer, 0, n);
+
+                        //Estado de la descarga en progreso
+                        total += n;
+                        if (tamañoRecurso != -1) {
+
+                            Integer porc = (total * 100) / tamañoRecurso;
+                            Bundle b1 = new Bundle();
+                            b1.putString("ServicioDescarga", String.valueOf(porc));
+                            mReceiver.send(this.PROGRESS, b1);
+
+                        } else {
+                            Integer porc = total;
+                            Bundle b1 = new Bundle();
+                            b1.putString("ServicioDescarga", String.valueOf(porc));
+                            mReceiver.send(this.PROGRESS, b1);
+
+                        }
+
+                    }//while
+
+                    //cerramos los Streams
+                    bos.close();
+                    is.close();
+
+                    byte[] arrayImagen = bos.toByteArray();
+                    imagenBmpDescargada = BitmapFactory.decodeByteArray(arrayImagen, 0, arrayImagen.length);
+
+                    Log.d(TAG, "FINALIZA la descarga de la imagen");
+                    Bundle b2 = new Bundle();
+                    b2.putString("ServicioDescarga", "Finalizada la descarga");
+                    b2.putParcelable("bitmapImagen", imagenBmpDescargada);
+                    mReceiver.send(this.FINSISHED, b2);
+
+                }else {
+
+                }
+                    // TODO: Devolver la URI de la imagen si todo ha ido bien.
+
+                    // TODO: Controlar los casos en los que no ha ido bien: excepciones en las conexiones, etc...
+                } catch(UnsupportedEncodingException e){
+                    e.printStackTrace();
+                } catch(ProtocolException e){
+                    e.printStackTrace();
+                } catch(MalformedURLException e){
+                    e.printStackTrace();
+                } catch(IOException e){
+                    e.printStackTrace();
+                }
+
+            } else {
+
+                Log.d(TAG, "Servicio NO Correcto! Intent = null");
+            }
+
+        }
+
+        public String leerJson (JsonReader reader)throws IOException {
+
+            String url = null;
+
+            reader.beginObject();
+            while (reader.hasNext()) {
+
+                String name = reader.nextName();
+                switch (name) {
+                    case "img":
+                        url = reader.nextString();
+                        break;
+                    default:
+                        reader.skipValue();
+                        break;
+                }
+            }
+            reader.endObject();
+
+            return url;
+        }
+
+
+
     }
-}
